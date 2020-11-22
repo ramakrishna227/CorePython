b99214572a87a60615c6ab7d96af31d2e9a0c3d5
Rama Krishna Kurisetti <ramakrishna.kurisetti@blueyonder.com>
2020-11-22 23:49:57 +0530

    practice

--- fibonacci.py
+++ fibonacci.py
@@ -3,6 +3,7 @@ while b < 1000:
     print(b, end=' ')
     # a, b = b, a + b
     # some how above line is equivalent to below
+    # In python, expressions on the right hand side are evaluated before assigning the results to left hand side variables
     c = b
     b = a + b
     a = c
